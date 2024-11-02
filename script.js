24fcaf85fa92fb31c4d668635dc5ddef972390e5
KeitaHidaka <keitinn.reader@gmail.com>
2024-11-02 23:27:07 +0900

    reCAPTCHA v3 実装

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-let submitted = false;
-function onRecaptchaSuccess(token) {
-  document.getElementById('submit-button').style.display = 'block';
-}
+async function sendForm() {
+  // 送信ボタンを無効化
+  const submitBtnElement = document.querySelector('input[type=submit]');
+  submitBtnElement.disabled = true;
 
-function onRecaptchaExpired() {
-  document.getElementById('submit-button').style.display = 'none';
-}
+  // メッセージを更新
+  const messageElement = document.getElementById('message');
+  messageElement.textContent = '送信中...';
 
-function onSubmit(token) {
-  document.getElementById('inquiry-form').submit();
-}
+  // reCAPTCHA による検証を実行
+  const token = await grecaptcha.execute('6LcmPnMqAAAAAGqRReD710SZAqR7gcfcL0YJ4uOX', {
+    action: 'submit',
+  });
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('submit-button').style.display = 'none';
-});
+  // フォームデータに reCAPTCHA のトークンを追加
+  const formData = new FormData(document.getElementById('inquiry-form'));
+  formData.append('g_recaptcha_response', token);
+
+  // フォームデータをバックエンドへ送信
+  let req;
+  try {
+    req = await fetch(
+      'https://us-central1-recaptchatest-440109.cloudfunctions.net/functionReCAPTCHA',
+      {
+        method: 'POST',
+        body: new URLSearchParams(formData),
+      }
+    );
+  } catch (e) {
+    // エラー処理
+    messageElement.textContent = 'エラーが発生しました';
+
+    // 送信ボタンを有効化
+    submitBtnElement.disabled = false;
+    return;
+  }
+
+  // // レスポンスを受け取り、画面に表示
+  // messageElement.textContent = await req.text();
+  const response = await req;
+  if (response.ok) {
+    window.location.href = './thanks.html';
+  } else {
+    messageElement.textContent = await req.text();
+  }
+
+  // 送信ボタンを有効化
+  submitBtnElement.disabled = false;
+}
